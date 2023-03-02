dea2a1d6542ac798dbed97c340624bebc2d0f7b9
Sakib <sakibatreus@gmail.com>
2023-03-02 21:56:19 +0600

    API Recap with Phone Hunter

--- Milestone 6 : Intermediate JavaScript, API/Module 35 : API Recap with Phone Hunter/js/app.js
+++ Milestone 6 : Intermediate JavaScript, API/Module 35 : API Recap with Phone Hunter/js/app.js
@@ -1,14 +1,32 @@
-const loadPhones = async(searchText) => {
+const loadPhones = async(searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
-    phones =  phones.slice(0,10);
+    // display 10 phones only
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+        phones =  phones.slice(0,10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+    
+    // display no phone found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+    // display all phones
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -18,18 +36,67 @@ const displayPhones = phones => {
                     <div class="card-body">
                       <h5 class="card-title">${phone.phone_name}</h5>
                       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailsModal">Show Details</button>
                     </div>
                 </div>
         `;
         phoneContainer.appendChild(phoneDiv);
     })
+    // stop loader
+    toggleSpinner(false);
 }
 
-
-document.getElementById('btn-search').addEventListener('click', function(){
+const processSearch = (dataLimit) => {
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText)
+    loadPhones(searchText, dataLimit)
+}
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    processSearch(10);
 ;})
 
-loadPhones();
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        processSearch(10);
+    }
+})
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+// not the best way to show all
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+;})
+
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailsModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails =  document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
+        <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No storage information'}</p>
+        <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Found'}</p>
+        <p>ChipSet: ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'ChipSet Not Found'}</p>
+        <ul>Sensors: <li>${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : 'Sensors Not Found'}</li></ul>
+    `
+}
+
+loadPhones('apple');
